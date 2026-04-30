11bd6c0f8880cc674c7a7f733125cd5a4c0451d3
Mohammad Umar <umararshad0.ua@gmail.com>
2026-04-30 17:35:16 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
 
+const api = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL + '/api'
+});
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
   if (token) cfg.headers.Authorization = `Bearer ${token}`;
